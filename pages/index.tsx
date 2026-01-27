823291203b518cc54df8252cf8ab5d4dfc1a5c06
shox17 <159898318+shox17@users.noreply.github.com>
2026-01-27 14:32:27 +0900

    feat: publish main layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,17 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
+    <Stack className={"home-page"}>
+      <Stack>
+        <Stack className="container">Popular Properties</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
       </Stack>
     </Stack>
   );
 };
-export default withLayoutMain(Home); //
+export default withLayoutMain(Home);
