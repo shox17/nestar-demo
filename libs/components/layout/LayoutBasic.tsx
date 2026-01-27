a06dbd058028389ed66a100dcd805ac770ea97a3
shox17 <159898318+shox17@users.noreply.github.com>
2026-01-27 14:43:17 +0900

    feat: publish basic layout components

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -1,24 +1,44 @@
 import { Stack } from "@mui/material";
 import Head from "next/head";
+import Top from "../Top";
+import Footer from "../Footer";
 
 const withLayoutBasic = (Component: any) => {
   return (props: any) => {
     return (
-      <>
-        <Head>
-          <title>Nestar</title>
-        </Head>
-        <Stack id="pc-wrap">
-          <Stack sx={{ background: "#81c784" }}>Header Basic</Stack>
+        <>
+          <Head>
+            <title>Nestar</title>
+          </Head>
+          <Stack id="pc-wrap">
+            <Stack id={"top"}>
+              <Top />
+            </Stack>
 
-          <Stack id={"main"}>
-            <Component {...props} />
+            <Stack
+              className={"header-basic"}
+              style={{
+                backgroundImage: `url(/img/banner/properties.png)`,
+                backgroundSize: "cover",
+                boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
+              }}
+            >
+              <Stack className={"container"}>
+                <strong>Property Search</strong>
+                <span>We are glad to see you again!</span>
+              </Stack>
+            </Stack>
+
+            <Stack id={"main"}>
+              <Component {...props} />
+            </Stack>
+
+            <Stack id={"footer"}>
+              <Footer />
+            </Stack>
           </Stack>
-
-          <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
-        </Stack>
-      </>
-    );
+        </>
+      );
   };
 };
 
