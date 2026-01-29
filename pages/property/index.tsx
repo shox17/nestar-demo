149da42addbfa87386aa2245602f14337fda883b
shox17 <159898318+shox17@users.noreply.github.com>
2026-01-29 13:19:53 +0900

    feat: property list publishing

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,11 +1,49 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Stack } from "@mui/material";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import Filter from "@/libs/components/property/Filter";
+import PropertyCard from "@/libs/components/property/PropertyCard";
 
 const PropertyList: NextPage = () => {
+  const [properties, setProperties] = useState<number[]>([1, 2, 3, 4, 5, 6]);
+
   return (
-    <div style={{ margin: "20px 0px" }}>
-      <Stack className="container">Property List</Stack>
+    <div id={"property-list-page"} style={{ position: "relative" }}>
+      <Stack className={"container"}>
+        <Box className={"right"}>
+          <span>Sort by</span>
+          <div>
+            <Button endIcon={<KeyboardArrowDownRoundedIcon />}>New</Button>
+          </div>
+        </Box>
+        <Stack className={"property-page"}>
+          <Stack className={"filter-config"}>
+            <Filter />
+          </Stack>
+          <Stack className={"main-config"} mb={"76px"}>
+            <Stack className={"list-config"}>
+              {properties.map((property, index) => {
+                return <PropertyCard key={index} />;
+              })}
+            </Stack>
+            <Stack className={"pagination-config"}>
+              <Stack className={"pagination-box"}>
+                <Pagination
+                  page={1}
+                  count={5}
+                  shape="circular"
+                  color="primary"
+                />
+              </Stack>
+              <Stack className={"total-result"}>
+                <Typography>Total 5 properties available</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
     </div>
   );
 };
